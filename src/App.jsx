3c9be10a06a6174c95faa22d48dd1e79ae1d1590
Andres <baguiboy@gmail.com>
2025-08-04 17:52:02 -0600

    Colores y estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1360,13 +1360,13 @@ const App = () => {
                 </div>
             )}
     
-            <nav className="bg-gradient-to-r from-yellow-800 to-yellow-500 shadow-md sticky top-0 z-[60]">
+            <nav className="bg-gradient-to-r from-yellow-400 to-yellow-500 shadow-md sticky top-0 z-[60]">
                 <div className="container mx-auto px-4 py-4 flex flex-col md:flex-row justify-between items-center">
                     <div className="flex items-center space-x-3 cursor-pointer" onClick={() => handleNavigation('home')}>
                         <div>
                             <YucaguiaIcon />
                         </div>
-                        <span className="text-2xl font-bold" style={{ color: '#2363EB' }}>Yucaguia</span>
+                        <span className="text-2xl font-bold" style={{ color: '#3483FA' }}>Yucaguia</span>
                     </div>
                     <div className="flex items-center space-x-4 mt-2 md:mt-0">
                         <button
